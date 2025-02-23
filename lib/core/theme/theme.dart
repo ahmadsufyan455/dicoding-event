6f05ded3a0032667881e0944b5523cdabc535aab
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2025-02-23 21:04:26 +0800

    change font family to poppins

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,13 +1,16 @@
+import 'package:dicoding_event/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     brightness: Brightness.light,
     scaffoldBackgroundColor: const Color(0xFFF8F9FA),
+    fontFamily: FontFamily.poppins,
   );
 
   static ThemeData darkTheme = ThemeData(
     brightness: Brightness.dark,
     scaffoldBackgroundColor: const Color(0xFF121212),
+    fontFamily: FontFamily.poppins,
   );
 }
